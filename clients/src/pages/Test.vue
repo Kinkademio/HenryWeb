924a42fa400cf19d85cf36bcc7728f6042086beb
Kinka <katyamew@gmail.com>
2023-11-01 07:57:16 +1000

    **

--- clients/src/pages/Test.vue
+++ clients/src/pages/Test.vue
@@ -1,3 +1,3 @@
 <template>
-  <iframe src="api/uploads/index.html" style="height: 100%; width: 100%;"></iframe>
+  <iframe src="api/uploads/index.html" style="height: 100%; width: 100%; left: 0; top:0; position: fixed;"></iframe>
 </template>
